838c0cfd12dc3562b2c01922fe0a1af53bb7239c
zee <zeeshiq58@gmail.com>
2020-02-03 00:38:59 +0500

    setup graphql and show message
    setiup graphql and its schema and print the hello world message

--- server.js
+++ server.js
@@ -1,3 +1,26 @@
-const express = require('express')
+ const express = require('express')
  const expressGraphQl = require('express-graphql')
+ const{
+ 	GraphQLSchema,
+ 	GraphQLObjectType,
+ 	GraphQLString
+ }=require('graphql')
+ const schema = new GraphQLSchema({
+ 	query: new GraphQLObjectType({
+ 		name:'HelloWorld',
+ 		fields:()=>({
+ 			message:{
+ 				type: GraphQLString,
+ 				resolve:() => 'Hello World'
+ 			}
+ 			
+ 		})
+ 	})
+ })
+ const app = express()
+
+ app.use('/graphql',expressGraphQl({
+ 	schema:schema,
+ 	graphiql:true
+ }))
  app.listen(5000,()=>console.log('Server Running'))
